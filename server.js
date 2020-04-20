fb030a05b228a9d506240b49ab3b2a81e31f5967
Ellie Day <heyellieday@gmail.com>
2020-04-19 19:25:09 -0500

    Add error handling for invalid file

--- server.js
+++ server.js
@@ -11,6 +11,8 @@ const cors = require('cors')
 const PORT = 8080;
 const HOST = '0.0.0.0';
 
+const formatResponse = (output) => `<html><body><pre><code>${output}</code></pre></body></html>`;
+
 // App
 const app = express();
 app.use(cors({credentials: true, origin: true}))
@@ -18,9 +20,13 @@ app.get('/verify', async (req, res) => {
     if (!req.query.url) return res.send("provide a url param");
     console.log(req.query.url);
     await exec(`curl ${req.query.url} -o file.asice`);
-    const result = await exec('/usr/local/bin/digidoc-tool open file.asice')
+    try {
+        const result = await exec('/usr/local/bin/digidoc-tool open file.asice')
+        res.send(formatResponse(result.stdout));
+    } catch(e) {
+        res.send(formatResponse(e.message));
+    }
     await exec(`rm file.asice`);
-    res.send(`<html><body><pre><code>${result.stdout}</code></pre></body></html>`);
 });
 
 app.listen(PORT, HOST);
